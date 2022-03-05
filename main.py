6e75fc709141c4a7d809b04e5b9f94d11c8c0fdc
Maria <mashasalimova777@gmail.com>
2022-03-05 16:21:09 +0500

    task 4

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from data.__all_models import User, Jobs
 global_init('database.db')
 session = create_session()
 user = User()
-user.surname = "Boris"
+"""user.surname = "Boris"
 user.name = "Olegov"
 user.age = 10
 user.position = "child"
@@ -15,15 +15,16 @@ user.email = "boris_olegov@mars.org"
 user.hashed_password = "mars_is_cool"
 session.add(user)
 session.commit()
-session.close()
-"""job = Jobs()
+session.close()"""
+
 job = Jobs()
 job.team_leader = 1
-job.job = 'deployment of residential modules 1 and 2'
-job.work_size = 15
-job.collaborators = '2, 3'
+job.job = 'biologist'
+job.work_size = 20
+job.collaborators = '2'
 job.is_finished = False
 session.add(job)
-session.commit()"""
+session.commit()
+session.close()
 job = session.query(Jobs).first()
 print(job, job.team_leader_instance)
